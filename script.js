e3ad2c371624b00feac2751129d4684ba9074627
Agathatzz <117759649+Agathatzz@users.noreply.github.com>
2024-11-15 10:10:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const ctx = document.getElementById('socialMediaChart').getContext('2d');
 
-// Dados fictícios para o gráfico
+// Dados atualizados para o gráfico
 const data = {
-  labels: ['Facebook', 'YouTube', 'WhatsApp', 'Instagram', 'WeChat', 'TikTok'],
+  labels: ['Facebook', 'YouTube', 'WhatsApp', 'Instagram', 'TikTok'],
   datasets: [{
     label: 'Usuários (em bilhões)',
-    data: [2.96, 2.74, 2.24, 2.0, 1.26, 1.05],
+    data: [2.96, 2.74, 2.24, 2.0, 1.05],
     backgroundColor: [
       '#9c27b0',
       '#ba68c8',
       '#ce93d8',
       '#d1c4e9',
-      '#f3e5f5',
       '#ab47bc'
     ],
     borderColor: '#4a0072',
